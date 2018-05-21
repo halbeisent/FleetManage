1b0e0d462c7cf70729569c80b8ff96ac77ddcdd9
halbeisent <dev@halbeisen.fr>
2018-05-21 22:24:30 +0200

    Ajout d'un système de 'ALPHA' de vues de véhicules, ce projet utilisera Materialize

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Initialisation du dropdown et du sidenav en mode responsive de Materialize
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+  $(".dropdown-trigger").dropdown();
+  $('.slider').slider();
+});
